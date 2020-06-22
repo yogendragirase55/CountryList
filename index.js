65c71a0f9b99544505b24df154d6ed09be6006c5
yogendragirase55 <47450325+yogendragirase55@users.noreply.github.com>
2020-06-22 18:08:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import CountrySelection from './src/CountryList/CountrySelection';
+import { countries } from './src/CountryList/Constants';
+
 export { CountrySelection };
+
+
+export function generateSectionData(code) {
+    let country = {};
+    for (var i = 0; i < countries.length; i++) {
+        if (countries[i].code == code) {
+            country = countries[i];
+            break;
+        }
+    }
+    return country;
+}
