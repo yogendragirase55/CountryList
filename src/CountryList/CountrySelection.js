bf13c2b7e74a8c4bae60511bf1503a01fd025c4a
yogendragirase55 <47450325+yogendragirase55@users.noreply.github.com>
2020-06-22 14:10:07 +0530

    Update File
    - Search country code and name

--- src/CountryList/CountrySelection.js
+++ src/CountryList/CountrySelection.js
@@ -58,7 +58,7 @@ export default class CountrySelection extends React.Component {
    * Change search text action
    */
   onChangeSearchText = (text) => {
-    const filtered = countries.filter(country => country.name.toLowerCase().indexOf(text.toLowerCase()) > -1);
+    const filtered = countries.filter(country => (country.name.toLowerCase().indexOf(text.toLowerCase()) && country.callingCode.indexOf(text.toLowerCase())) > -1);
     this.generateSectionData(filtered);
   }
 
@@ -97,6 +97,7 @@ export default class CountrySelection extends React.Component {
           </View>
         </View>
         <SectionList
+        keyboardShouldPersistTaps={'handled'}
           renderItem={({ item, index, section }) => (
             <ItemView
               item={item}
